4a8d427e62399a1909dddf5211dcdc39b7c22e19
Julian David Nuñez <juliandavidnunesfranco@hotmail.es>
2022-08-27 17:28:27 -0500

    Configuration BoilerPlate

--- api/src/db.js
+++ api/src/db.js
@@ -33,7 +33,7 @@ sequelize.models = Object.fromEntries(capsEntries);
 const { Videogame } = sequelize.models;
 
 // Aca vendrian las relaciones
-// Product.hasMany(Reviews);
+// Product.hasMany(Reviews); ***********
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
