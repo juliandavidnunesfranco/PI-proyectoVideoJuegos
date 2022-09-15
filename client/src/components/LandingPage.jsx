6e1f4a3e2c3cc50b644b2e99ac723cfd68593da3
Milton Lopez <miltonmarinol@gmail.com>
2022-09-14 19:18:32 -0500

    feat: push.

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -1,16 +1,16 @@
-import  React  from "react";
-import { Link } from 'react-router-dom';
-import './LandingPage.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./LandingPage.css";
 
 function LandingPage() {
-    return (
-        <div>
-          <h2 className = 'landingfont'> Welcome to Project Video-Games</h2>
-            <Link to='/home'>
-              <button className='buttonLanding'>Start</button>
-            </Link>
-        </div>
-    )
-};
-  
+  return (
+    <div className="landingfont">
+      <h2> Welcome to Project Video-Games</h2>
+      <Link to="/home">
+        <button className="buttonLanding">Start</button>
+      </Link>
+    </div>
+  );
+}
+
 export default LandingPage;
